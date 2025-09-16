7e3e6f94f7484f441aa3992f7c94546e9729290d
Govardhan  Shemane <140930185+GovardhanShemane@users.noreply.github.com>
2025-09-16 12:25:07 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+
 const express = require('express');
 const apiRoutes = require('./routes/apiRoutes');
 const app = express();
@@ -5,6 +6,12 @@ const app = express();
 app.use(express.json());
 app.use('/api', apiRoutes);
 
+// Root route 
+app.get('/', (req, res) => {
+  res.send('Lead Scoring Backend is running!');
+});
+
+// Error handling middleware
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).json({ message: 'Something went wrong!' });
